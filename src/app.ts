f1fa7d36661e47981efb718e4aa6d519bf68d73a
roadaspen <709208680@qq.com>
2024-07-22 17:01:07 +0800

    feat: 添加类express的routes

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,24 @@
 // 引入http模块
 import ejs from "ejs";
-import http from "http";
-import url from "url";
+import expressApp from "./modules/express-routes";
 
-import { staticPath } from "./modules/routes";
-http
-  .createServer(function (req, res) {
-    staticPath(req, res, "static");
-    const pathname = req.url ? url.parse(req.url).pathname : "/";
-    if (pathname === "/news") {
-      res.writeHead(200, { "Content-Type": "text/html;charset='utf-8'" });
-      const query = req.url ? url.parse(req.url, true).query : {};
-      console.log(query);
-      let msg = "数据库里面获取的数据";
-      const list = [
-        {
-          title: "新闻1",
-        },
-        {
-          title: "新闻2",
-        },
-        {
-          title: "新闻3",
-        },
-        {
-          title: "新闻4",
-        },
-      ];
-      ejs.renderFile(
-        "./src/views/news.ejs",
-        { user: { name: "嘿嘿" }, msg, newsList: list, page: query.page },
-        (err, data) => {
-          res.end(data);
-        }
-      );
-    } else if (pathname === "/login") {
-      ejs.renderFile("./src/views/login.ejs", {}, (err, data) => {
-        res.end(data);
-      });
-    } else if (pathname === "/doLogin") {
-      let data = "";
-      req.on("data", (chunk) => {
-        data += chunk;
-      });
-      req.on("end", () => {
-        console.log("data", data);
-        res.end("post ok");
-      });
-    } else if (pathname === "/registry") {
-      res.writeHead(200, { "Content-Type": "text/html;charset='utf-8'" });
-      res.write("<head><meta charset='utf-8'></head>");
-      res.end("注册");
-    } else if (pathname === "/admin") {
-      res.writeHead(200, { "Content-Type": "text/html;charset='utf-8'" });
-      res.write("<head><meta charset='utf-8'></head>");
-      res.end("处理后的业务逻辑");
-    } else {
-      res.writeHead(404, { "Content-Type": "text/html;" });
-      res.write("<head><meta charset='utf-8'></head>");
-      res.end("这个页面不存在");
-    }
-  })
-  .listen(8081);
+const app = new expressApp();
+app.static("static");
+app.get("/login", (req, res) => {
+  ejs.renderFile("./src/views/login.ejs", {}, (err, data) => {
+    res.end(data);
+  });
+});
+
+app.listen(8081);
+
+app.post("/doLogin", (req, res) => {
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk;
+  });
+  req.on("end", () => {
+    console.log("data", data);
+    res.end("post ok");
+  });
+});
---
+++ src/modules/express-routes.ts
@@ -0,0 +1,98 @@
+/** 实现一个类似于express的路由,封装一个路由注册器 */
+import fs from "fs";
+import http from "http";
+import path from "path";
+import url from "url";
+import { getFileMime } from "./common";
+
+export default class expressApp {
+  private routes: {
+    [key: string]: (
+      req: http.IncomingMessage,
+      res: http.ServerResponse<http.IncomingMessage> & {
+        req: http.IncomingMessage;
+      }
+    ) => void;
+  };
+  private staticPath: string;
+  constructor() {
+    this.routes = {};
+    this.staticPath = "static";
+  }
+  private init = (port: number) => {
+    const that = this;
+    http
+      .createServer(function (req, res) {
+        let pathname = url.parse(req.url || "").pathname || "/";
+        that.initStatic(req, res, pathname);
+        pathname = `${req.method} ${pathname}`;
+        const route = that.routes[pathname];
+        try {
+          console.log("pathname", pathname);
+          route(req, res);
+        } catch (error) {
+          res.writeHead(404, { "Content-Type": "text/html;charset='utf-8'" });
+          res.end("not found");
+        }
+      })
+      .listen(port);
+  };
+  private initStatic = (
+    req: http.IncomingMessage,
+    res: http.ServerResponse<http.IncomingMessage> & {
+      req: http.IncomingMessage;
+    },
+    pathname: string
+  ) => {
+    const urls = `../${this.staticPath || ""}` + pathname;
+    console.log("urls", urls);
+    if (pathname !== "/favicon.ico") {
+      try {
+        const isFile = fs.statSync(urls);
+        console.log("isFile", isFile);
+        if (isFile.isFile()) {
+          const data = fs.readFileSync(urls);
+          if (data) {
+            const extName = path.extname(urls);
+            const mime = getFileMime(extName.slice(1));
+            res.writeHead(200, {
+              "Content-Type": `${mime};charset='utf-8'`,
+            });
+            /** 结束相应 */
+            res.end(data);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  public static = (path: string) => {
+    this.staticPath = path;
+  };
+  public get = (
+    path: string,
+    callback: (
+      req: http.IncomingMessage,
+      res: http.ServerResponse<http.IncomingMessage> & {
+        req: http.IncomingMessage;
+      }
+    ) => void
+  ) => {
+    this.routes[`GET ${path}`] = callback;
+  };
+  public post = (
+    path: string,
+    callback: (
+      req: http.IncomingMessage,
+      res: http.ServerResponse<http.IncomingMessage> & {
+        req: http.IncomingMessage;
+      }
+    ) => void
+  ) => {
+    this.routes[`POST ${path}`] = callback;
+  };
+  public listen(port: number) {
+    this.init(port);
+  }
+}
